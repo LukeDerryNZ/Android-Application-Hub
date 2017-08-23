c24bed67a54ecd5ef4a55fb3645f3b71357b0e42
LukeDerryNZ <LukeDerryNZ@gmail.com>
2017-08-23 14:24:43 +1200

    Initial commit

---
+++ app/src/main/java/com/lukederrynz/android_test/TextToSpeechActivity.java
@@ -0,0 +1,13 @@
+package com.lukederrynz.android_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextToSpeechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_to_speech);
+    }
+}
