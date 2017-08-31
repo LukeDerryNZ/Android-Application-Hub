9c7758a68bbf1124ad66d9c29e87eb246e469246
LukeDerryNZ <lukederrynz@gmail.com>
2017-08-31 19:52:22 +1200

    Basic functionality for calculator now working utilising a stack Added clear function and UI to calculator. Tidied up logic, switch cases, listeners.

--- app/src/main/java/com/lukederrynz/application_Hub/CalculatorActivity.java
+++ app/src/main/java/com/lukederrynz/application_Hub/CalculatorActivity.java
@@ -10,13 +10,14 @@ import android.widget.Toast;
 
 import com.lukederrynz.android_test.R;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class CalculatorActivity extends AppCompatActivity {
 
     private TextView textView;
-
-    private Double tempAnswer;
-
     private Calculator calculator;
+    private Deque<String> equationStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
 
         calculator = new Calculator();
+        equationStack = new ArrayDeque<>();
 
         initializeControls();
     }
@@ -44,18 +46,24 @@ public class CalculatorActivity extends AppCompatActivity {
         // Create number button events
         GridLayout numGridLayout = (GridLayout)findViewById(R.id.Calculator_numbersGridLayout);
         for(int i=0; i<numGridLayout.getChildCount(); i++) {
+            // Cache button
             Button tempButton = (Button)numGridLayout.getChildAt(i);
-            // create new button event for this button
-            numGridLayout.getChildAt(i).setOnClickListener(new myListener(tempButton.getText().toString()) {
+            // Create new listener passing string as parameter
+            tempButton.setOnClickListener(new myListener(tempButton.getText().toString()) {
                 @Override public void onClick(View v) {
-                    switch (this.value) {
-                        case "=":
-                            calculateEquation();
-                            break;
-                        default:
-                            Toast.makeText(CalculatorActivity.this, "Number "+this.value, Toast.LENGTH_SHORT).show();
-                            enterNumber(this.value);
-                            break;
+                    if (this.value.equals("=")) {
+                        switch (equationStack.size()) {
+                            case 2:
+                                // If textView is not empty
+                                if (!isTextViewEmpty()) {
+                                    equationStack.push(getValueAsString());
+                                }
+                            case 3:
+                                textView.setText(String.valueOf(calcEquation()));
+                                equationStack.clear();
+                        }
+                    } else {
+                        enterNumber(this.value);
                     }
                 }
             });
@@ -64,58 +72,68 @@ public class CalculatorActivity extends AppCompatActivity {
         // Create operator button events
         GridLayout operatorGridLayout = (GridLayout)findViewById(R.id.Calculator_operatorsGridLayout);
         for(int i=0; i<operatorGridLayout.getChildCount(); i++) {
-            Button tempButton = (Button)operatorGridLayout.getChildAt(i);
+            final Button tempButton = (Button)operatorGridLayout.getChildAt(i);
             // Create a new event
             operatorGridLayout.getChildAt(i).setOnClickListener(new myListener(tempButton.getText().toString()) {
                 @Override public void onClick(View v) {
 
-                    // Check textView is not empty
-                    if (!isTextViewEmpty()) {
+                    // Bail if textView is empty - Note that this disallows negatives
+                    if (isTextViewEmpty()) return;
 
-                        // Save current textView value
-                        tempAnswer = getValueAsDouble();
-                        // And clear
-                        cleartextView();
+                    switch (equationStack.size()) {
+                        case 0:
+                            // Push textView val
+                            equationStack.push(getValueAsString());
+                            // And operator
+                            equationStack.push(this.value);
+                            cleartextView();
+                            break;
 
-                        double result;
-                        Calculator.MathOperation<Double> command = null;
+                        case 1:
+                            Toast.makeText(CalculatorActivity.this, "ERROR:STACK SIZE=1", Toast.LENGTH_SHORT).show();
+                            break;
 
-                        // Call correct operator event
-                        switch (this.value) {
+                        case 2:
+                            // If peek is operator, replace operator - this resolves multiple operators issue
+                            if ( isOperator(equationStack.peek()) ) {
+                                equationStack.pop();
+                                equationStack.push(this.value);
+                            }
 
-                            case "X":
-                                if (command == null) command = calculator.new MultiplyOperation();
-                                break;
-                            case "+":
-                                if (command == null) command = calculator.new AddOperation();
-                                break;
-                            case "-":
-                                if (command == null) command = calculator.new SubtractOperation();
-                                break;
-                            case "/":
-                                if (command == null) command = calculator.new DivideOperation();
-                                break;
-                            default:
-                                Toast.makeText(CalculatorActivity.this, "ERROR: Operator Not Recognized", Toast.LENGTH_SHORT).show();
-                        }
-                        // and calculate result
+                            // Push value to stack
+                            equationStack.push(getValueAsString());
 
-                        /*
-                            Here I need to append to a list or stack perhaps.
-                            We want [ 1stVal 1stOp 2ndVal 2ndOp ]
-                            At each Operator, append textVal to list.
-                            Then append operator.
-                            2         +       2                  *                     4            =
-                                 store 2,+               calc(2+2),store 4
-                                 count 2                count 3 so calc sum
-                                                         remove index[0],[1]
-                         */
-                        result = command.execute(tempAnswer, getValueAsDouble());
+                            // And clear textView
+                            cleartextView();
 
-                        Toast.makeText(CalculatorActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+                            // Proceed to case 3
 
-                        // Set textView
-                        textView.setText(String.valueOf(result));
+                        case 3:
+                            // Process the first equation
+                            double v1 = Double.valueOf(equationStack.pop());
+                            String op = equationStack.pop();
+                            double v2 = Double.valueOf(equationStack.pop());
+
+                            // Calculate equation and push to stack
+                            switch(op) {
+                                case "+":
+                                    equationStack.push( String.valueOf(calculator.new AddOperation().execute(v1, v2)));
+                                    break;
+                                case "-":
+                                    equationStack.push( String.valueOf(calculator.new SubtractOperation().execute(v1, v2)));
+                                    break;
+                                case "X":
+                                    equationStack.push( String.valueOf(calculator.new MultiplyOperation().execute(v1, v2)));
+                                    break;
+                                case "/":
+                                    equationStack.push( String.valueOf(calculator.new DivideOperation().execute(v1, v2)));
+                                    break;
+                                default:
+                                    Toast.makeText(CalculatorActivity.this, "Invalid Operator", Toast.LENGTH_SHORT).show();
+                                    return;
+                            }
+                            // Finally add operator
+                            equationStack.push(this.value);
                     }
 
                 }
@@ -125,9 +143,42 @@ public class CalculatorActivity extends AppCompatActivity {
 
     }
 
-    // Parse textView String as Double and return
-    private double getValueAsDouble() {
-        return Double.parseDouble(textView.getText().toString());
+    // Calculate valueOne Operator valueTwo
+    // Note that NO other equation format is compatible
+    private double calcEquation() {
+
+        cleartextView();
+        // Process the first equation
+        double v1 = Double.valueOf(equationStack.pop());
+        String op = equationStack.pop();
+        double v2 = Double.valueOf(equationStack.pop());
+
+
+        // Calculate equation and push to stack
+        switch(op) {
+            case "+":
+                return calculator.new AddOperation().execute(v1, v2);
+            case "-":
+                return calculator.new SubtractOperation().execute(v1, v2);
+            case "X":
+                return calculator.new MultiplyOperation().execute(v1, v2);
+            case "/":
+                return calculator.new DivideOperation().execute(v1, v2);
+            default:
+                Toast.makeText(CalculatorActivity.this, "Invalid Operator", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        // TODO //
+        return v1; // ERROR - Just return v1 for now
+
+    }
+
+    private String getValueAsString() {
+        return textView.getText().toString();
+    }
+
+    private boolean isOperator(String s) {
+        return (s.equals("=") || s.equals("+") || s.equals("X") || s.equals("/"));
     }
 
     // Check null textView String. Return true if empty
@@ -140,24 +191,9 @@ public class CalculatorActivity extends AppCompatActivity {
         textView.setText("");
     }
 
-    // Clear All
-    private void clearAll() {
-        cleartextView();
-        tempAnswer = 0.0;
-    }
-
     // Enter number into textView
     private void enterNumber(String numStr) {
         textView.append(numStr);
-    }
-
-    // Calculate equation
-    private void calculateEquation() {
-
-        // Get number in memory
-
-
-        Toast.makeText(CalculatorActivity.this, "Calculating Equals...", Toast.LENGTH_SHORT).show();
     }
 
 }
