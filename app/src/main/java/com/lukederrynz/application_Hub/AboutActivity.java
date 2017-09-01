f5b1085ff82baef2512c2213ec2ade7e194d4fc0
LukeDerryNZ <lukederrynz@gmail.com>
2017-09-01 12:56:36 +1200

    New Activity

---
+++ app/src/main/java/com/lukederrynz/application_Hub/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.lukederrynz.application_Hub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lukederrynz.application_hub.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
