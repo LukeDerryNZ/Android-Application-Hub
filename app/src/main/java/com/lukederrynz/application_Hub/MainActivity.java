20590e1ab1018ae5af20758b142c0e132395f265
LukeDerryNZ <lukederrynz@gmail.com>
2017-09-07 13:24:54 +1200

    Removed commented methods

--- app/src/main/java/com/lukederrynz/application_Hub/MainActivity.java
+++ app/src/main/java/com/lukederrynz/application_Hub/MainActivity.java
@@ -13,52 +13,20 @@ public class MainActivity extends AppCompatActivity {
     String entryMsg = "Android : ";
     public static boolean DEBUG = true;
 
-    // region Android On Events
-
 //    // Called when the activity is first created.
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (DEBUG) Log.d(entryMsg, "onCreate()");
     }
-//
-//    // Called when the activity is about to become visible.
-//    @Override protected void onStart() {
-//        super.onStart();
-//        if (DEBUG) Log.d(entryMsg, "onStart()");
-//    }
-//
-//    // Called when the activity has become visible.
-//    @Override protected void onResume() {
-//        super.onResume();
-//        if (DEBUG) Log.d(entryMsg, "onResume()");
-//    }
-//
-//    // Called when another activity takes focus.
-//    @Override protected void onPause() {
-//        super.onPause();
-//        if (DEBUG) Log.d(entryMsg, "onPause");
-//    }
-//
-//    // Called when activity is no longer visible.
-//    @Override protected void onStop() {
-//        super.onStop();
-//        if (DEBUG) Log.d(entryMsg, "onStop()");
-//    }
-//
-//    // Called just before activity is destroyed.
-//    @Override protected void onDestroy() {
-//        super.onDestroy();
-//        if (DEBUG) Log.d(entryMsg, "onDestroy()");
-//    }
-
-    //endregion
 
     // Start the Text To Speech Activity
     public void startActivityTTS(View view) {
         Intent i = new Intent(this, TextToSpeechActivity.class);
         startActivity(i);
     }
+
+    //region START ACTIVITY METHODS
 
     // Start the Weather Activity
     public void startActivityWeather(View view) {
@@ -83,4 +51,7 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, DropperActivity.class);
         startActivity(i);
     }
+
+    //endregion
+
 }
